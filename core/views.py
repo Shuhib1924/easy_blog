c5dfd4c39e78397a9ff4be462c104e399f43d4c5
Shuhib Siddiqi <shuhib_s@live.de>
2024-02-17 07:43:42 +0100

    blog works connection works

---
+++ core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from blogs.models import Blog, Category
+from assignments.models import About
+
+
+def home(request):
+    featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by(
+        "updated_at"
+    )
+    posts = Blog.objects.filter(is_featured=False, status="Published")
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+    context = {
+        "featured_posts": featured_posts,
+        "posts": posts,
+        "about": about,
+    }
+    return render(request, "home.html", context)
